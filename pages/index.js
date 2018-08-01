e40a1e5f0fdcad5b3909666759e51d4018880c21
Cole Bemis <colebemis@gmail.com>
2018-08-01 00:49:22 -0700

    Add query input

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,40 @@
-function IndexPage() {
-  return <div>Hello Dasher</div>
+import { withRouter } from 'next/router'
+import { stringify } from 'query-string'
+import React, { Component } from 'react'
+
+class IndexPage extends Component {
+  state = {
+    query: this.props.router.query.query || ''
+  }
+
+  handleQueryChange = event => {
+    this.setState({ query: event.target.value })
+  }
+
+  handleQuerySubmit = event => {
+    event.preventDefault()
+
+    const { router } = this.props
+    const { query } = this.state
+
+    if (query !== router.query.query) {
+      router.push(`/?${stringify({ ...router.query, query })}`)
+    }
+  }
+
+  render() {
+    const { router } = this.props
+    const { query } = this.state
+
+    return (
+      <div>
+        <form onSubmit={this.handleQuerySubmit}>
+          <input value={query} onChange={this.handleQueryChange} />
+          {query !== router.query.query ? <button>Apply</button> : null}
+        </form>
+      </div>
+    )
+  }
 }
 
-export default IndexPage
+export default withRouter(IndexPage)
