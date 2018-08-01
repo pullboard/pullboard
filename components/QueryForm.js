1dd68ffee9ff30d4a94d2f722b55df959ee9cbd8
Cole Bemis <colebemis@gmail.com>
2018-08-01 07:34:38 -0700

    Create QueryForm component

---
+++ components/QueryForm.js
@@ -0,0 +1,38 @@
+import { withRouter } from 'next/router'
+import { stringify } from 'query-string'
+import React, { Component } from 'react'
+
+class QueryForm extends Component {
+  state = {
+    query: this.props.router.query.query || ''
+  }
+
+  handleChange = event => {
+    this.setState({ query: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    const { router } = this.props
+    const { query } = this.state
+
+    if (query !== router.query.query) {
+      router.push(`/?${stringify({ ...router.query, query })}`)
+    }
+  }
+
+  render() {
+    const { router } = this.props
+    const { query } = this.state
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input value={query} onChange={this.handleChange} />
+        {query !== router.query.query ? <button>Apply</button> : null}
+      </form>
+    )
+  }
+}
+
+export default withRouter(QueryForm)
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,14 @@
-import { withRouter } from 'next/router'
-import { stringify } from 'query-string'
 import React, { Component } from 'react'
+import QueryForm from '../components/QueryForm'
 
 class IndexPage extends Component {
-  state = {
-    query: this.props.router.query.query || ''
-  }
-
-  handleQueryChange = event => {
-    this.setState({ query: event.target.value })
-  }
-
-  handleQuerySubmit = event => {
-    event.preventDefault()
-
-    const { router } = this.props
-    const { query } = this.state
-
-    if (query !== router.query.query) {
-      router.push(`/?${stringify({ ...router.query, query })}`)
-    }
-  }
-
   render() {
-    const { router } = this.props
-    const { query } = this.state
-
     return (
       <div>
-        <form onSubmit={this.handleQuerySubmit}>
-          <input value={query} onChange={this.handleQueryChange} />
-          {query !== router.query.query ? <button>Apply</button> : null}
-        </form>
+        <QueryForm />
       </div>
     )
   }
 }
 
-export default withRouter(IndexPage)
+export default IndexPage
