08a443025a5e0bbc7077a59bb6c0d185359aa2b9
Cole Bemis <colebemis@gmail.com>
2018-08-05 00:15:16 -0700

    Report code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
   testURL: 'http://localhost',
+  collectCoverageFrom: [
+    '<rootDir>/pages/**/*.{js,jsx}',
+    '<rootDir>/components/**/*.{js,jsx}',
+  ],
 }
