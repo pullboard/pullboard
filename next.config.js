4fb31e9117bd21f67d4090625cfccfc50fb4b968
Cole Bemis <colebemis@gmail.com>
2018-08-08 11:34:48 -0700

    Move config data into env variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
   serverRuntimeConfig: {
-    githubClientSecret: '466ad0b81b77eff2586ecbf5552a035f6ca0d542',
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
   },
   publicRuntimeConfig: {
-    githubClientId: '4a840730a7e2b1b153dd',
-    redirectUri: 'http://localhost:3000/callback',
+    githubClientId: process.env.GITHUB_CLIENT_ID,
+    redirectUri: process.env.REDIRECT_URI,
   },
 }
