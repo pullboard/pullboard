e00af62e1e18e3678cd666a4dea7bdf0a852b206
Cole Bemis <colebemis@gmail.com>
2018-08-08 10:31:13 -0700

    Use Router instead of window.location

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Router from 'next/router'
 import { isLoggedIn } from '../lib/auth'
 import { getGithubAuthUrl } from '../lib/github'
 import { redirect } from '../lib/utils'
@@ -16,7 +17,7 @@ class LoginPage extends Component {
     return (
       <div>
         <h1>Welcome to PullBoard</h1>
-        <button onClick={() => (window.location = getGithubAuthUrl())}>
+        <button onClick={() => Router.push(getGithubAuthUrl())}>
           Continue with GitHub
         </button>
       </div>
